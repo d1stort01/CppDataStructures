cf7914ce9034523bd4fba91384ff818cb33cc7d4
d1stort <wangjiaqi02@qq.com>
2025-10-11 15:23:08 +0800

    Experiment 1

---
+++ Exp-1/Exp-1/SqList.hpp
@@ -0,0 +1,132 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+constexpr int initcap = 5;
+template <typename T>
+class SqList
+{
+public:
+    T* data;
+    int capacity;
+    int length;
+
+    SqList() {
+        data = new T[initcap];
+        capacity = initcap;
+        length = 0;
+    }
+
+    SqList(const SqList<T>& s) {
+        capacity = s.capacity;
+        length = s.length;
+        data = new T[capacity];
+        for (int i = 0; i < length; i++) {
+            data[i] = s.data[i];
+        }
+    }
+
+    ~SqList() {
+        delete[] data;
+        data = nullptr;
+    }
+
+    void recap(int newcap) {
+        if (newcap <= 0) {
+            return;
+        }
+        T* olddata = data;
+        data = new T[newcap];
+        capacity = newcap;
+        for (int i = 0; i < length; i++) {
+            data[i] = olddata[i];
+        }
+        delete[] olddata;
+    }
+
+    void CreateList(T a[], int n) {
+        length = 0;
+        for (int i = 0; i < n; i++) {
+            if (length == capacity) {
+                recap(2 * length);
+            }
+            data[length] = a[i];
+            length++;
+        }
+    }
+
+    void Add(T e) {
+        if (length == capacity) {
+            recap(2 * length);
+        }
+        data[length] = e;
+        length++;
+    }
+
+    int GetLength() {
+        return length;
+    }
+
+    bool GetElem(int i, T& e) {
+        if (i < 0 || i >= length) {
+            return false;
+        }
+        e = data[i];
+        return true;
+    }
+
+    bool SetElem(int i, T e) {
+        if (i < 0 || i >= length) {
+            return false;
+        }
+        data[i] = e;
+        return true;
+    }
+
+    int GetNo(T e) {
+        int i = 0;
+        while (i < length&& data[i] != e) {
+            i++;
+        }
+        if (i >= length) {
+            return -1;
+        }
+        return i;
+    }
+
+    bool Insert(int i, T e) {
+        if (i<0 || i>length) {
+            return false;
+        }
+        if (length == capacity) {
+            recap(2 * length);
+        }
+        for (int j = length; j > i; j--) {
+            data[j] = data[j - 1];
+        }
+        data[i] = e;
+        length++;
+        return true;
+    }
+
+    bool Delete(int i) {
+        if (i < 0 || i > length) {
+            return false;
+        }
+        for (int j = i; j < length - 1; j++) {
+            data[j] = data[j + 1];
+        }
+        length--;
+        if (capacity > initcap && length <= capacity / 4) {
+            recap(capacity / 2);
+        }
+        return true;
+    }
+
+    void DispList() {
+        for (int i = 0; i < length; i++) {
+            cout << data[i] << " ";
+        }
+        cout << endl;
+    }
+};
--- Exp-1/Exp-1/main.cpp
+++ Exp-1/Exp-1/main.cpp
@@ -1,74 +1,125 @@
 #include "SqList.hpp"	//引用顺序表类 
 #include <vector>
+#include <iostream>
+using namespace std;
 
 // 第七题
+void differenceSqList(SqList<int>& A, SqList<int>& B, SqList<int>& C) {
+    if (C.length != 0) {
+        return;
+    }
+    for (int i = 0; i < A.GetLength(); i++) {
+        int elem;                   // A复制到C
+        A.GetElem(i, elem);
+        C.Add(elem);                
+    }
+    for (int i = 0; i < B.GetLength(); i++) {
+        int elem;
+        B.GetElem(i, elem);         // 遍历B
+        int pos = C.GetNo(elem);
+        if (pos != -1) {            // C中存在B的元素则删除
+            C.Delete(pos);
+        }
+    }
+}
+
+void differenceVector(vector<int>& A, vector<int>& B, vector<int>& C) {
+    C = A;
+    for (int i = 0; i < B.size(); i++) {
+        auto it = find(C.begin(), C.end(), B[i]);
+        if (it != C.end()) {
+            C.erase(it);
+        }
+    }
+}
 
 // 第八题
+void intersectionSqList(SqList<int>& A, SqList<int>& B, SqList<int>& C) {
+    if (C.length != 0) {
+        return;
+    }
+    for (int i = 0; i < A.GetLength(); i++) {
+        int elem;                       // 遍历A
+        A.GetElem(i, elem);
+        if (B.GetNo(elem) != -1) {      // B中存在A的元素则加入C
+            C.Add(elem);
+        }
+    }
+}
+
+void intersectionVector(vector<int>& A, vector<int>& B, vector<int>& C) {
+    C.clear();
+    for (int i = 0; i < A.size(); i++) {
+        if (find(B.begin(), B.end(), A[i]) != B.end()) {
+            C.push_back(A[i]);
+        }
+    }
+}
 
 void Disp(vector<int>& myv)		       //输出vector的元素
 {
-	vector<int>::iterator it;
-	for (it = myv.begin(); it != myv.end(); it++)
-		cout << *it << " ";
-	cout << endl;
+    vector<int>::iterator it;
+    for (it = myv.begin(); it != myv.end(); it++)
+        cout << *it << " ";
+    cout << endl;
 }
 
 int main()
 {
-	int a1[] = { 1, 3, 5, 7 };
-	int b1[] = { 1, 2, 4, 5, 9 };
-	int a2[] = { 1, 3, 2 };
-	int b2[] = { 5, 1, 4, 2 };
+    int a1[] = { 1, 3, 5, 7 };
+    int b1[] = { 1, 2, 4, 5, 9 };
+    int a2[] = { 1, 3, 2 };
+    int b2[] = { 5, 1, 4, 2 };
 
-	SqList<int> A, B, C1, C2;  //建立类型为int的顺序表对象
-	vector<int> Av, Bv, C1v, C2v;
+    SqList<int> A, B, C1, C2;  //建立类型为int的顺序表对象
+    vector<int> Av, Bv, C1v, C2v;
 
-	cout << "创建整数顺序表" << endl;
-	A.CreateList(a1, 4);
-	B.CreateList(b1, 5);
-	Av.assign(a1, a1 + sizeof(a1) / sizeof(a1[0]));
-	Bv.assign(b1, b1 + sizeof(b1) / sizeof(b1[0]));
+    cout << "创建整数顺序表" << endl;
+    A.CreateList(a1, 4);
+    B.CreateList(b1, 5);
+    Av.assign(a1, a1 + sizeof(a1) / sizeof(a1[0]));
+    Bv.assign(b1, b1 + sizeof(b1) / sizeof(b1[0]));
 
-	cout << "SqList: " << endl;
-	A.DispList();
-	B.DispList();
-	cout << "Vecotr: " << endl;
-	Disp(Av);
-	Disp(Bv);
+    cout << "SqList: " << endl;
+    A.DispList();
+    B.DispList();
+    cout << "Vector: " << endl;
+    Disp(Av);
+    Disp(Bv);
 
-	cout << "\nSqList差集结果" << endl;
-	difference(A, B, C1);
-	C1.DispList();
+    cout << "\nSqList差集结果" << endl;
+    differenceSqList(A, B, C1);
+    C1.DispList();
 
-	cout << "Vecotr差集结果" << endl;
-	difference(Av, Bv, C1v);
-	C1.DispList();
+    cout << "Vector差集结果" << endl;
+    differenceVector(Av, Bv, C1v);
+    C1.DispList();
 
 
-	cout << "\n创建整数顺序表" << endl;
-	A.CreateList(a2, 3);
-	B.CreateList(b2, 4);
+    cout << "\n创建整数顺序表" << endl;
+    A.CreateList(a2, 3);
+    B.CreateList(b2, 4);
 
-	Av.assign(a2, a2 + sizeof(a2) / sizeof(a2[0]));
-	Bv.assign(b2, b2 + sizeof(b2) / sizeof(b2[0]));
+    Av.assign(a2, a2 + sizeof(a2) / sizeof(a2[0]));
+    Bv.assign(b2, b2 + sizeof(b2) / sizeof(b2[0]));
 
-	cout << "SqList: " << endl;
-	A.DispList();
-	B.DispList();
-	cout << "Vecotr: " << endl;
-	Disp(Av);
-	Disp(Bv);
+    cout << "SqList: " << endl;
+    A.DispList();
+    B.DispList();
+    cout << "Vector: " << endl;
+    Disp(Av);
+    Disp(Bv);
 
-	intersection(A, B, C2);
-	intersection(Av, Bv, C2v);
+    intersectionSqList(A, B, C2);
+    intersectionVector(Av, Bv, C2v);
 
-	cout << "\nSqList交集结果" << endl;
-	C2.DispList();
+    cout << "\nSqList交集结果" << endl;
+    C2.DispList();
 
-	cout << "\Vecotr交集结果" << endl;
-	Disp(C2v);
+    cout << "Vector交集结果" << endl;
+    Disp(C2v);
 
-	return 0;
+    return 0;
 }
 
 
